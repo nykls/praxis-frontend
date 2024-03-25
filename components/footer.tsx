efe27490611d9c0373ec9c501cbda1c78cc2788b
nykls <niklas.eulitz@gmail.com>
2024-03-25 09:49:10 +0100

    add github

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,10 +6,12 @@ import Maps from "./maps";
 import Image from "next/image";
 import { Suspense } from "react";
 import { MapsSkeleton } from "./skeletons";
+import { Github } from "lucide-react";
+import { Button } from "./ui/button";
 
 const Footer = () => {
   return (
-    <footer className="bg-secondary border-t py-7 mt-7 ">
+    <div className="bg-secondary border-t py-7 mt-7 ">
       <FullWidthWrapper>
         <div className="pb-5">
           <Suspense fallback={<MapsSkeleton />}>
@@ -25,7 +27,7 @@ const Footer = () => {
           </div>
 
           {/* Image */}
-          <div className="w-full lg:w-auto mb-4 lg:mb-0 flex justify-center">
+          <div className="w-full lg:w-auto mb-4 lg:mb-0 flex">
             <div className="flex justify-center items-center space-x-4">
               <div className="relative h-20 w-20">
                 <Image src="/bvo_kinderosteopathie.png" alt="Logo" fill />
@@ -39,15 +41,28 @@ const Footer = () => {
           {/* Links */}
           <div className="w-full lg:w-auto flex justify-center lg:justify-end items-center">
             <Link href="#" className="text-sm mx-2">
-              Impressum
+              <Button className="p-0" variant="link">
+                Impressum
+              </Button>
             </Link>
             <Link href="#" className="text-sm mx-2">
-              Datenschutzbestimmungen
+              <Button className="p-0" variant="link">
+                Datenschutzbestimmungen
+              </Button>
+            </Link>
+            <Link href="#" className="text-sm mx-2">
+              <Button
+                className="p-0 hover:text-primary size-10"
+                variant="ghost"
+              >
+                {" "}
+                <Github />
+              </Button>
             </Link>
           </div>
         </div>
       </FullWidthWrapper>
-    </footer>
+    </div>
   );
 };
 
