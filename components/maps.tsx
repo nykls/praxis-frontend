0d045dc9c3225186a3593f84e414991bd2a551fc
nykls <niklas.eulitz@gmail.com>
2024-03-29 10:18:41 +0100

    Update maps.tsx

--- components/maps.tsx
+++ components/maps.tsx
@@ -1,25 +1,7 @@
 "use client";
 
-import { Separator } from "@/components/ui/separator";
-import {
-  APIProvider,
-  AdvancedMarker,
-  ControlPosition,
-  Map,
-  MapControl,
-} from "@vis.gl/react-google-maps";
-import Link from "next/link";
-import { Card, CardContent, CardDescription, CardHeader } from "./ui/card";
-import { Button } from "./ui/button";
-import { Suspense } from "react";
-import { MapsSkeleton } from "./skeletons";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { Compass, PhoneOutgoing, Route } from "lucide-react";
 import { GoogleMapsEmbed } from "@next/third-parties/google";
+import { Card } from "./ui/card";
 
 export default function Maps() {
   const position = { lat: 51.097900607863075, lng: 13.677138946517001 };
