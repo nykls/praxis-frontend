22e8047cdd6ba801c46c2c62bc1d944942ae8bbd
nykls <niklas.eulitz@gmail.com>
2024-03-31 08:54:53 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -3,9 +3,11 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      allow: ["/", "/blog", "/blog/*, /about"],
+      userAgent: ["Googlebot", "Applebot", "Bingbot"],
+      allow: ["/"],
       disallow: ["/studio", "/imprint", "/privacy"],
     },
+
     sitemap: process.env.NEXT_PUBLIC_URL + "sitemap.xml",
   };
 }
