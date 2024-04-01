999b3d0ba67637d16316c80e5053e4b989d1dd75
nykls <niklas.eulitz@gmail.com>
2024-04-01 09:25:21 +0200

    add dynamic sitemap generation

---
+++ app/sitemap.tsx
@@ -0,0 +1,40 @@
+import { MetadataRoute } from "next";
+import { client } from "@/sanity/lib/client";
+
+const query = `*[_type == "post"]
+      {
+        slug,
+        publishedAt
+      }
+      `;
+
+export default async function sitemap() {
+  const res = await client.fetch(query);
+  return [
+    ...res.map(
+      (post: {
+        slug: { current: string };
+        publishedAt: string | number | Date;
+      }) => ({
+        url: `${process.env.NEXT_PUBLIC_URL}blog/${post.slug.current}`,
+        lastModified: new Date(post.publishedAt),
+        changeFrequency: "always",
+      })
+    ),
+    {
+      url: process.env.NEXT_PUBLIC_URL,
+      lastModified: new Date(),
+      changeFrequency: "always",
+    },
+    {
+      url: process.env.NEXT_PUBLIC_URL + "blog",
+      lastModified: new Date(),
+      changeFrequency: "always",
+    },
+    {
+      url: process.env.NEXT_PUBLIC_URL + "about",
+      lastModified: new Date(),
+      changeFrequency: "always",
+    },
+  ];
+}
