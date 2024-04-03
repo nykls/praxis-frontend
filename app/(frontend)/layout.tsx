ff6e792d928238903e76d69fa095ed4ef4a9b18b
nykls <niklas.eulitz@gmail.com>
2024-04-03 14:29:48 +0200

    change font

--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -7,10 +7,9 @@ import { cn } from "@/lib/utils";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
-import { Noto_Sans_Display } from "next/font/google";
 import "./globals.css";
-
-const noto = Noto_Sans_Display({ subsets: ["latin"], display: "swap" });
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 
 export const metadata: Metadata = {
   title: {
@@ -28,10 +27,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="de" suppressHydrationWarning>
-      <body
-        className={cn("h-full relative font-sans antialiased", noto.className)}
-      >
+    <html
+      lang="de"
+      className={`${GeistSans.variable} ${GeistMono.variable}`}
+      suppressHydrationWarning
+    >
+      <body className={cn("h-full relative font-sans antialiased")}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <header>
             <Navbar />
