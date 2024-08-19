c00d6ed9a0f91bda13617b0c727272195bfe2481
nykls <niklas.eulitz@gmail.com>
2024-08-19 19:54:43 +0200

    Update page.tsx

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -5,9 +5,13 @@ import Perks from "@/components/perks";
 
 export default function Home() {
   return (
-    <>
-      <Hero />
-      <Perks />
-    </>
+    <div className="">
+      <div className="overflow-hidden">
+        <Hero />
+      </div>
+      <div className="relative z-20">
+        <Perks />
+      </div>
+    </div>
   );
 }
