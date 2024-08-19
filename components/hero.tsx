9227f3979f9b59d54f7b390afda0cbf651c457b9
nykls <niklas.eulitz@gmail.com>
2024-08-19 19:53:51 +0200

    new hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,79 +1,94 @@
-"use client";
-
+import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import Balancer from "react-wrap-balancer";
 import ContactForm from "./contact";
 import { Button, buttonVariants } from "./ui/button";
+import { TextEffect } from "./animate/text-effect";
+import { WavyBackground } from "./ui/wavy-background";
+import Typography from "./typography";
+
+const containerVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+};
 
 export const Hero = () => {
   return (
-      <div className="h-screen -mt-20 w-full flex dark:bg-dot-white/[0.2] bg-dot-black/[0.2] items-center justify-center">
-        {/* Radial gradient for the container to give a faded look */}
-        <div className=" pointer-events-none flex [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <div className="flex w-full flex-col justify-center items-center text-center space-y-24 lg:space-y-60">
-          <div className="space-y-3 select-none">
-            <h1 className="sr-only">Praxis für Osteopathie, Yoga & Qigong Maitri Katrin Eulitz</h1>
+    <WavyBackground waveOpacity={1} speed="slow" waveWidth={10} blur={5}>
+      <div className="relative z-10 flex h-screen overflow-hidden w-full items-center justify-center">
+        <motion.div
+          className="flex fixed w-full max-w-4xl flex-col items-center justify-center space-y-12 px-3 text-center"
+          initial="hidden"
+          animate="visible"
+          variants={containerVariants}
+        >
+          <div className="space-y-3">
             <motion.h1
-              initial={{ opacity: 0.5, y: 50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{
-                delay: 0,
-                duration: 0.1,
-                ease: "easeOut",
-              }}
-              className="scroll-m-20 font-semibold text-xl lg:text-3xl"
+              variants={itemVariants}
+              className="text-lg font-semibold sm:text-4xl md:text-5xl"
             >
-              <Balancer>
-                Praxis für Osteopathie, Yoga & Qigong Maitri Katrin Eulitz
-              </Balancer>
+              Praxis für Osteopathie, Yoga & Qigong Maitri Katrin Eulitz
             </motion.h1>
             <motion.h2
-              initial={{ opacity: 0.5, y: 50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{
-                delay: 0.1,
-                duration: 0.1,
-                ease: "easeOut",
-              }}
-              className="bg-gradient-to-tr border-none from-primary dark:to-foreground to-foreground/75 bg-clip-text tracking-tight text-transparent scroll-m-20 pb-2 text-3xl lg:text-5xl font-extrabold first:mt-0"
+              variants={itemVariants}
+              className="
+              bg-gradient-to-r from-orange-500 via-yellow-500 to-green-600 
+              bg-clip-text text-4xl md:text-6xl font-bold tracking-tight 
+              text-transparent relative
+              drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)]
+              [text-shadow:_0_1px_0_rgb(0_0_0_/_25%)]
+              [filter:_drop-shadow(0_0_0.75rem_rgba(255,255,255,0.5))]
+            "
             >
-              <Balancer>Herzlich Willkommen!</Balancer>
+              Herzlich Willkommen!
             </motion.h2>
-            <motion.p
-              initial={{ opacity: 0.5, y: 50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{
-                delay: 0.2,
-                duration: 0.1,
-                ease: "easeOut",
-              }}
-              className="text-center text-sm lg:text-xl tracking-tight font-bold md:px-5 px-1"
-            >
-              <Balancer>
-                Willkommen in der Praxis für Osteopathie, Yoga & Qigong Maitri Katrin Eulitz.
-                Wir bieten osteopathische Behandlungen für alle Altersgruppen,
-                von Babys bis Erwachsene, sowie Yoga- und Qigong-Kurse an. Die
-                Kostenübernahme durch Krankenkassen ist möglich.
-              </Balancer>
-            </motion.p>
+            <Balancer>
+              <TextEffect
+                as="p"
+                preset="blur"
+                per="word"
+                className="mx-auto max-w-2xl text-base font-medium sm:text-lg md:text-xl"
+              >
+                Willkommen in unserer Praxis. Wir bieten osteopathische
+                Behandlungen für alle Altersgruppen sowie Yoga- und Qigong-Kurse
+                an. Kostenübernahme durch Krankenkassen möglich.
+              </TextEffect>
+            </Balancer>
           </div>
-          <div className="flex flex-col mx-auto w-60 lg:w-auto gap-4 sm:flex-row">
+          <motion.div
+            className="flex flex-col gap-4 sm:flex-row sm:gap-6"
+            variants={itemVariants}
+          >
             <ContactForm>
               <Button aria-haspopup="dialog" aria-controls="contactFormDialog">
                 Kontakt
               </Button>
             </ContactForm>
-            <span className="sr-only">Mehr Informationen</span>
             <Link
               href="#service"
               className={buttonVariants({ variant: "outline" })}
             >
-              Mehr &rarr;
+              Mehr <span aria-hidden="true">&rarr;</span>
               <span className="sr-only">Mehr Informationen</span>
             </Link>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </div>
+    </WavyBackground>
   );
 };
+
+export default Hero;
